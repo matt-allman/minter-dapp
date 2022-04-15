ff0a7464f59d10e60828ab12db07cf8aced35f09
Matt Allman <mattallman@hotmail.com>
2022-04-15 12:24:35 +0100

    updated colour

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,10 +58,10 @@ const MINT_PRICE = 0.02; // Minting price per NFT. Rinkeby = ETH, Polygon = MATI
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-04-14T12:00:48+01:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-04-28T12:00:48+01:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-04-08T12:00:48+01:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-16T12:00:48+01:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xb7Fd707C8f295180D88b82d1Ac6753B012BCb1D0"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
